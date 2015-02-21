8378ff4a9c83d8a025509f5d1bcf9d8157fced2b
CarNg <Car@Cars-Mac-Mini.local>
2015-02-21 23:21:19 +0000

    Revert "Initial Push"
    This reverts commit 99a55cc372b11a4d75d54ed7294c89314dc3fddb.

--- Assets/Scripts/Ball Game/BoardRotate.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class BoardRotate : MonoBehaviour {
-
-	void Update () {
-		transform.Rotate (0, Input.GetAxis ("Horizontal") * -100 * Time.deltaTime, 0, Space.Self);
-	}
-}
